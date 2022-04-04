383ca8bb4524742d3dfd851acaadeaec62d7de5b
MarkVed17 <vedantlahane17@gmail.com>
2022-04-04 21:09:39 +0530

    added a temporary link to notes-screen

--- src/frontend/screens/HomeScreen/HomeScreen.js
+++ src/frontend/screens/HomeScreen/HomeScreen.js
@@ -1,12 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const HomeScreen = () => {
   return (
     <div
       className="main-content"
-      style={{ color: "white", marginTop: "10rem", textAlign: "center" }}
+      style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "1rem", color: "white", marginTop: "10rem" }}
     >
-      Home Screen
+      <p>Home Screen</p>
+      <Link
+        to="/notes"
+        style={{
+          color: "black",
+          border: "1px solid yellow",
+          padding: "8px",
+          width: "fit-content",
+          textDecoration: "none",
+          backgroundColor: "yellow"
+        }}
+      >
+        Go to Notes
+      </Link>
     </div>
   );
 };
