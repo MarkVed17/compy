1bd20f661d16d96629fdebea9521edb507c1eb04
MarkVed17 <vedantlahane17@gmail.com>
2022-04-05 17:55:34 +0530

    feat: added Note Editor component

--- src/frontend/components/index.js
+++ src/frontend/components/index.js
@@ -1,3 +1,6 @@
 export { Navbar } from "./Navbar/Navbar";
 export { Footer } from "./Footer/Footer";
 export { Sidebar } from "./Sidebar/Sidebar";
+export { PrivateRoute } from "./PrivateRoute/PrivateRoute";
+export { RichTextEditor } from "./RichTextEditor/RichTextEditor";
+export { NoteEditor } from "./NoteEditor/NoteEditor";
