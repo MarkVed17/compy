07b046b2b4e54f3024dd43284c3ab4d66776576d
MarkVed17 <vedantlahane17@gmail.com>
2022-04-07 01:43:59 +0530

    added constants

---
+++ src/frontend/constants/noteConstants.js
@@ -0,0 +1,6 @@
+export const TITLE = "TITLE";
+export const PINNED = "PINNED";
+export const LABEL = "LABEL";
+export const CONTENT = "CONTENT";
+export const COLOR = "COLOR";
+export const RESET = "RESET";
