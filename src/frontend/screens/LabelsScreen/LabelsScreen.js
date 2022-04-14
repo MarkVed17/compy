144d267f88ba66c3c0b849d1f591b68c4798d629
MarkVed17 <vedantlahane17@gmail.com>
2022-04-14 21:52:00 +0530

    updated Labels Screen

--- src/frontend/screens/LabelsScreen/LabelsScreen.js
+++ src/frontend/screens/LabelsScreen/LabelsScreen.js
@@ -1,16 +1,71 @@
 import React from "react";
-import { Sidebar } from "../../components";
+import { Sidebar, NoteCard } from "../../components";
+import { useNotes } from "../../contexts";
 import "./LabelsScreen.css";
 
 const LabelsScreen = () => {
+  const { notes, setNotes } = useNotes();
+  const labelOneNotes = notes
+    .filter(
+      (note) =>
+        note.label === "Label 1" &&
+        note.isPinned === true &&
+        note.trash === false
+    )
+    .concat(
+      notes.filter(
+        (note) =>
+          note.label === "Label 1" &&
+          note.isPinned === false &&
+          note.trash === false
+      )
+    );
+  const labelTwoNotes = notes
+    .filter(
+      (note) =>
+        note.label === "Label 2" &&
+        note.isPinned === true &&
+        note.trash === false
+    )
+    .concat(
+      notes.filter(
+        (note) =>
+          note.label === "Label 2" &&
+          note.isPinned === false &&
+          note.trash === false
+      )
+    );
   return (
     <div className="labels-container">
       <Sidebar />
-      <div
-        className="main-content"
-        style={{ color: "white", marginTop: "10rem", textAlign: "center" }}
-      >
-        Labels Screen
+      <div className="labelled-notes-list">
+        {labelTwoNotes.length || labelOneNotes.length > 0 ? (
+          <div>
+            {labelOneNotes.length > 0 && (
+              <div>
+                <h1 className="labelled-list-title">Label 1 Notes :</h1>
+                {labelOneNotes.map((note) => (
+                  <NoteCard key={note._id} note={note} setNotes={setNotes} />
+                ))}
+              </div>
+            )}
+
+            {labelTwoNotes.length > 0 && (
+              <div>
+                <h1 className="labelled-list-title">Label 2 Notes :</h1>
+                {labelTwoNotes.map((note) => (
+                  <NoteCard key={note._id} note={note} setNotes={setNotes} />
+                ))}
+              </div>
+            )}
+          </div>
+        ) : (
+          <div>
+            <h1 className="labelled-list-title">
+              Labeled Notes Will Be Visible Here
+            </h1>
+          </div>
+        )}
       </div>
     </div>
   );
